ee2e429eef5dedcb0c72ce57de99cb89054c7971
Francisca <franciscag.w.4@gmail.com>
2023-05-24 09:38:30 -0300

    clase 11

---
+++ Clase 11/main.js
@@ -0,0 +1,9 @@
+/*CLASE 11: Storage and json */
+//local storage guarda mas tiempo el session storage se va
+
+console.log (localStorage);
+
+//localStorage.setItem("nombre","francisca") //auque se reinicia se guarda y queda toda la informacion guardada
+
+let nombre = localStorage.getItem ("nombre");
+console.log (nombre);
