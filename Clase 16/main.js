2f1b49b84c52b93df8f1a5531e2995ea87d6bf9f
Francisca <franciscag.w.4@gmail.com>
2023-06-11 17:46:56 -0300

    probando cosas nuevas

---
+++ Clase 16/main.js
@@ -0,0 +1,6 @@
+//ajax y fetch clase 16
+
+//http conectarse al servidor, peticiones http
+
+//parametros: info adiccional (queryparams y url params )
+
--- Entrega/main.js
+++ Entrega/main.js
@@ -12,10 +12,13 @@ const productos = [
 
 
 const bagsContainer = document.getElementById("bagsContainer")
-const carrito = [];
-JSON.parse(localStorage.getItem("carrito")) || [];
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
 const carritoContainer = document.getElementById ("carrito");
 
+
+//recorro el array de los productos y los presento en el html
 const recorridoProductos = () =>{
     productos.forEach (producto =>{
         const card = document.createElement ("div");
@@ -25,39 +28,45 @@ const recorridoProductos = () =>{
         <h2>${producto.nombre}</h2>
         <h3>${producto.precio} USD</h3>
         <button id="select-${producto.id}">Select</button>
+        <hr/>
         `;
 
-        bagsContainer.append(card);
+        bagsContainer.append(card); //agrego el contenedor al html
 
         const selectButton = document.getElementById (`select-${producto.id}`);
         selectButton.addEventListener("click", ()=>{
             agregarAlCarrito(producto.id);
-            recorridoCarrito();
-        })
+        })//creas la funcion en el boton y al clikear se agrega al carrito
 
         const imgChange = document.getElementById(`move-${producto.id}`);
         imgChange.addEventListener("mouseover",()=>{
-            imgChange.scr = producto.imagenUrlChange;
+            imgChange.src = producto.imagenUrlChange;
         })
         imgChange.addEventListener("mouseout", ()=>{
-            imgChange.scr = producto.imagenUrl;
+            imgChange.src = producto.imagenUrl;
         })
     })
 }
 recorridoProductos ();
 
 
+//recorrer el carrito con los itemes agregados y mostrarlo
 const recorridoCarrito = () =>{
     carritoContainer.innerHTML="";
     carrito.forEach (item =>{
         const carritoFinal = document.createElement("ul");
         carritoFinal.innerHTML = ` 
-        <li>${item.nombre},cantidad: ${item.cantidad}, precio: ${item.precio}</li>
+        <li>${item.nombre},cantidad: ${item.cantidad}, precio: ${item.precio * item.cantidad}</li>
+        <button id="vaciar-${item.id}">Eliminar producto</button>
         `;
+       const deleteButton = document.getElementById (`vaciar${item.id}`);
+       deleteButton.addEventListener("click", ()=>{
+            eliminarProducto();
+        })
         carritoContainer.append(carritoFinal);
     })
 }
-
+//agregar al carrito o iniciar item si es que no esta
 const agregarAlCarrito = (productoId) =>{
     const item = productos.find((producto)=>producto.id === productoId);
     const productoEnCarrito = carrito.some((item)=> item.id===productoId);
@@ -65,13 +74,27 @@ const agregarAlCarrito = (productoId) =>{
 
     if (productoEnCarrito){
         item.cantidad++;
-        console.log(carrito);
     }else {
         item.cantidad = 1;
         carrito.push(item);
-        console.log(carrito);
     }
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    recorridoCarrito ();
 }
 
-localStorage.setItem("carrito",JSON.stringify(carrito));
-
+//funcion de eliminar prodcuto
+/*const deleteCarrito = (productoId) => {
+    const item = productos.find((producto)=>producto.id === productoId);
+    if (producto.cantidad === 1){
+        eliminarProducto (producto.id);
+    } else {
+        producto.cantidad--;
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    }
+    recorridoCarrito();
+}*/
+const eliminarProducto = (id) => {
+    carrito = carrito.filter ((producto)=>producto.id !== id);
+    localStorage.setItem ("carrito", JSON.stringify(carrito));
+    recorridoCarrito ();
+}
